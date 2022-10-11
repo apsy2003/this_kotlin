5e213a5550487fc7fcebad8874595029aa754ad7
apsy2003 <apsy2003@naver.com>
2022-10-11 14:15:42 +0900

    Int

--- Function/app/src/main/java/com/apsy2003/function/MainActivity.kt
+++ Function/app/src/main/java/com/apsy2003/function/MainActivity.kt
@@ -30,6 +30,8 @@ class MainActivity : AppCompatActivity() {
         Log.d("fun", "5와 7을 더한 결과값은 ${plus(5,7)}입니다.")
         Log.d("fun", "0부터 100을 모두 더한 결과값은 ${sum(100)}입니다.")
         printString("문자열")
+
+
     }
     //1. 반환값이 있는 함수
     fun square(x: Int): Int {
@@ -43,10 +45,30 @@ class MainActivity : AppCompatActivity() {
     fun getPi(): Double {
         return 3.14
     }
+
+    //문제 1.
+    fun plus(x: Int y: Int) : Int {
+        return x + y // <--square 함수는 입력받은 값에 2를 곱해서 반환한다.
+    }
+
+    //문제 2.
+    fun sum(x: Int) : Int{
+        var result = 0
+        for(num in 0..x){
+            result += num
+        }
+        return result
+    }
+    //문제 3.
+    fun printString(word: String){
+        System.out.println(word)
+    }
+
     //7. 기본값을 갖는 함수
     fun newFunction(name:String, age:Int = 29, weight:Double = 65.5){
         Log.d("fun","name의 값은 ${name}입니다.")
         Log.d("fun","age의 값은 ${age}입니다.")
         Log.d("fun","weight의 값은 ${weight}입니다.")
     }
+
 }
