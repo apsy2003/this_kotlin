f677f0351d5d05e20007a93f0c6d53f325096ce7
apsy2003 <apsy2003@naver.com>
2022-10-19 18:41:38 +0900

    mobileapp

---
+++ MobileApp/app/src/main/java/com/apsy2003/mobileapp/mobileapp2.kt
@@ -0,0 +1,11 @@
+package com.apsy2003.mobileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class mobileapp2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mobileapp2)
+    }
+}
