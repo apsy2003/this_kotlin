009511df879d3fbe3e6402627e7d6ba8b87c22a4
apsy2003 <apsy2003@naver.com>
2022-10-19 12:04:15 +0900

    ContainerRecyclerView

---
+++ ContainerRecyclerView/app/src/main/java/com/apsy2003/containerrecyclerview/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.apsy2003.containerrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.apsy2003.containerrecyclerview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val data: MutableList<Memo> = loadData()
+        var adapter = CustomAdapter()
+        adapter.listData = data
+        binding.recyclerView.adapter = adapter
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    //100개의 list를 가상으로 만듭니다.
+    fun loadData(): MutableList<Memo>{
+        val data: MutableList<Memo> = mutableListOf()
+        for (no in 1..100){
+            val title = "이것이 안드로이드다 ${no}"
+            val date = System.currentTimeMillis()
+            var memo = Memo(no, title, date)
+            data.add(memo)
+        }
+        return data
+    }
+}
