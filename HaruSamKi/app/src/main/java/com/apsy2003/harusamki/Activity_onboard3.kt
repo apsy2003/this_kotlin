2a481f9e03ad1618151078a5e25b6d05ac6ea1fc
apsy2003 <apsy2003@naver.com>
2022-10-20 17:41:21 +0900

    harusamki

---
+++ HaruSamKi/app/src/main/java/com/apsy2003/harusamki/Activity_onboard3.kt
@@ -0,0 +1,11 @@
+package com.apsy2003.harusamki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_onboard3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboard3)
+    }
+}
