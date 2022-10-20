1c0307d91c65094ded0b9bedc542e742e4a207e5
apsy2003 <apsy2003@naver.com>
2022-10-20 11:46:47 +0900

    FragmentDetail

--- Fragment/app/src/main/java/com/apsy2003/fragment/DetailFragment.kt
+++ Fragment/app/src/main/java/com/apsy2003/fragment/DetailFragment.kt
@@ -2,29 +2,33 @@ package com.apsy2003.fragment
 
 import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.apsy2003.fragment.databinding.FragmentListBinding
+import androidx.fragment.app.Fragment
+import com.apsy2003.fragment.databinding.FragmentDetailBinding
 
 
-class ListFragment : Fragment() {
-    var mainActivity: MainActivity? = null
+class DetailFragment : Fragment() {
+
+    lateinit var mainActivity: MainActivity
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-       //return inflater.inflate(R.layout.fragment_list, container, false)
-        val binding = FragmentListBinding.inflate(inflater, container, false)
-        binding.btnNext.setOnClickListener{mainActivity?.goDetail()}
+        //return inflater.inflate(R.layout.fragment_detail, container, false)
+        val binding = FragmentDetailBinding.inflate(inflater, container, false)
+        binding.btnBack.setOnClickListener {mainActivity.goBack()}
         return binding.root
     }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-
-        if(context is MainActivity) mainActivity = context
+        mainActivity = context as MainActivity
     }
+
+
 }
