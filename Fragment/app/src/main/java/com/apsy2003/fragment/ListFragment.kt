cf3a697c7c536aee5db05f56d2cc94db95486576
apsy2003 <apsy2003@naver.com>
2022-10-20 14:13:56 +0900

    FragmentSender

--- Fragment/app/src/main/java/com/apsy2003/fragment/ListFragment.kt
+++ Fragment/app/src/main/java/com/apsy2003/fragment/ListFragment.kt
@@ -8,23 +8,29 @@ import android.view.View
 import android.view.ViewGroup
 import com.apsy2003.fragment.databinding.FragmentListBinding
 
-
 class ListFragment : Fragment() {
     var mainActivity: MainActivity? = null
+    lateinit var binding:FragmentListBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-       //return inflater.inflate(R.layout.fragment_list, container, false)
-        val binding = FragmentListBinding.inflate(inflater, container, false)
-        binding.btnNext.setOnClickListener{mainActivity?.goDetail()}
+        binding = FragmentListBinding.inflate(inflater, container, false)
+
+        binding.btnNext.setOnClickListener { mainActivity?.goDeatail() }
+        binding.textTitle.text = arguments?.getString("key1")
+        binding.textValue.text = "${arguments?.getInt("key2")}"
         return binding.root
     }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
 
-        if(context is MainActivity) mainActivity = context
+        if (context is MainActivity) mainActivity = context
+    }
+
+    fun setValue(value:String) {
+        binding.textFromActivity.text = value
     }
 }
--- Fragment/app/src/main/java/com/apsy2003/fragment/MainActivity.kt
+++ Fragment/app/src/main/java/com/apsy2003/fragment/MainActivity.kt
@@ -2,35 +2,49 @@ package com.apsy2003.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.apsy2003.fragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    lateinit var listFragment: ListFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         setFragment()
+
+        binding.btnSend.setOnClickListener {
+            listFragment.setValue("전달할 값")
+        }
     }
 
-    fun setFragment(){
-        //ListFragment를 생성
-        val listFragment: ListFragment = ListFragment()
-        //프래그먼트 매니저를 통해서 트랜잭션을 시작하고, 시작한 트랜잭션을 변수에 저장
+    fun setFragment() {
+        listFragment = ListFragment()
+
+        var bundle = Bundle()
+        bundle.putString("key1", "List Fragment")
+        bundle.putInt("key2", 20220101)
+        listFragment.arguments = bundle
+
         val transaction = supportFragmentManager.beginTransaction()
-        //트랜잭션의 add()메서드로 frameLayout을 id로 가지고 있는 레이아웃에 앞에서 생성한 listFragment 삽입
         transaction.add(R.id.frameLayout, listFragment)
-        //setFragment()메서드 안에 다음과 같이 ListFragment를 생성
         transaction.commit()
     }
 
-    fun goDetail(){
+    fun goDeatail() {
         val detailFragment = DetailFragment()
+
         val transaction = supportFragmentManager.beginTransaction()
+
         transaction.add(R.id.frameLayout, detailFragment)
         transaction.addToBackStack("detail")
         transaction.commit()
     }
 
-    fun goBack(){
+    fun goBack() {
         onBackPressed()
     }
 }
