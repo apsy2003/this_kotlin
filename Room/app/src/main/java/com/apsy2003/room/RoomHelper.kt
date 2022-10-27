61bf0db351c074f3d4ee4e787f0c146de65cb9e8
apsy2003 <apsy2003@naver.com>
2022-10-27 12:57:23 +0900

    Database

---
+++ Room/app/src/main/java/com/apsy2003/room/RoomHelper.kt
@@ -0,0 +1,9 @@
+package com.apsy2003.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(RoomMemo::class), version = 1, exportSchema = false)
+abstract class RoomHelper: RoomDatabase() {
+    abstract fun roomMemoDao(): RoomMemoDao
+}
